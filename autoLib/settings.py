26eed9704ec2398600cc429d27a6c698b07024a9
Igorello-mobile <sidneff.igor@ya.ru>
2019-05-31 23:19:41 +0500

    add booksRecord to instslled apps in settings.py

--- autoLib/settings.py
+++ autoLib/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'booksRecord.apps.BooksrecordConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
