8984a52aba03a631d554b5b537acadcfdd454b0f
Igorello-mobile <sidneff.igor@ya.ru>
2019-06-16 17:51:28 +0500

    move the database from the SQLite to the MariaDB (mySQL) engine

--- autoLib/settings.py
+++ autoLib/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'autoLib.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'autoLib',
+        'USER': 'autoLib_user',
+        'PASSWORD': '2Hg?B`X}X$P<qAZ^',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
