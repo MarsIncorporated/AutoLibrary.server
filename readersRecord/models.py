dc6a7605887ba3e7ff4dbdde44f0e72ced15d3ac
Igorello-mobile <sidneff.igor@ya.ru>
2019-06-24 12:56:01 +0500

    move the GRADES tuple to core/__init__.py, add languages fields for the Student model and a little more

--- readersRecord/models.py
+++ readersRecord/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-import core.models
+import core
 
 class Student(core.models.Human):
     '''
@@ -18,15 +18,32 @@ class Student(core.models.Human):
     
     notes = models.TextField(
         max_length=500,
-        verbose_name="Заметки")
+        verbose_name="Заметки"
+    )
     
     grade = models.CharField(
         max_length=3,
-        choices=core.models.GRADES,
+        choices=core.GRADES,
         verbose_name="класс",
         help_text="номер и литера класса"
     )
     
+    first_lang = models.CharField(
+        max_length=2,
+        choices=core.LANGUAGES,
+        verbose_name='Первый язык',
+        blank=True,
+    )
+    
+    second_lang = models.CharField(
+        max_length=2,
+        choices=core.LANGUAGES,
+        verbose_name='Второй язык',
+        blank=True,
+    )
+    
     class Meta(core.models.Human.Meta):
+        ordering = ['id']
+        
         verbose_name = "ученик"
         verbose_name_plural = 'ученики'
