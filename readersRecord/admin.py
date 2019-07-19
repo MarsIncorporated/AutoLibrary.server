14d3cd449b310c61e6f0d49d8b36e12f2e4a4fa1
Igorello <sidneff.igor@ya.ru>
2019-07-19 16:01:38 +0500

    make the StudentAdmin class

--- readersRecord/admin.py
+++ readersRecord/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Student
 
-admin.site.register(Student)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "grade")
+    search_fields = ("second_name", 'first_name')
